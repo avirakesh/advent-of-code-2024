c82411e9d328578dabbcf09cfe4462675199044e
Avichal Rakesh <avichal.rakesh@gmail.com>
2024-12-01 17:12:13 -0800

    init CLI args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    // Day to run
+    #[arg(short, long)]
+    day: u32,
+
+    // Overriden Input
+    #[arg(short, long)]
+    input_file: std::path::PathBuf,
+}
+
 fn main() {
+    let args = Args::parse();
+    dbg!(args);
     println!("Hello, world!");
 }
